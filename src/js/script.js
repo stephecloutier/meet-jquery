f7c349c98253f9a037f957f38454ddb36edbc48f
stephecloutier <stephecloutier@gmail.com>
2017-02-13 16:39:22 +0100

    Exercise two

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,22 @@
 
+
+const fHandleTab = function ( oEvent ) {
+    let $this = $( this )
+
+    oEvent.preventDefault();
+    if ( $this.parent().hasClass( "active" ) ) {
+        return;
+    }
+    $( "ul.nav.nav-tabs .active" ).removeClass( "active" );
+    $this.parent().addClass( "active" );
+    $( ".tab-pane.active" ).removeClass( "active" );
+    $( `#${ $this.data( "tab-target" ) }` ).addClass( "active" );
+};
+
 $( function() {
     // 1. a with rel-external
     $( 'a[rel*="external"]' ).attr( "target", "_new" );
 
+    // 2. tabs
+    $( "ul.nav.nav-tabs a" ).on( "click", fHandleTab );
 } );
