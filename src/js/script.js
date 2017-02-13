a2ff664659ea1d03479b29193aa599785c359644
stephecloutier <stephecloutier@gmail.com>
2017-02-13 15:16:41 +0100

    Exercise two

--- src/js/script.js
+++ src/js/script.js
@@ -7,10 +7,38 @@
  */
 
 
+const fHandleTab = function ( oEvent ) {
+    oEvent.preventDefault();
+    let $elt = oEvent.currentTarget;
+
+    // check if current link is active
+    if ( $elt.parentNode.classList.contains( "active" ) ){
+        return;
+    }
+
+    // remove active class from old active
+    document.querySelector( "ul.nav.nav-tabs .active" ).classList.remove( "active" );
+
+    // add active class to current link
+    $elt.parentNode.classList.add( "active" );
+
+    // remove active class from old tab content
+    document.querySelector( ".tab-pane.active" ).classList.remove( "active" );
+
+    // add active class to current tab content
+    document.getElementById( $elt.getAttribute( "data-tab-target" ) ).classList.add( "active" );
+};
+
 window.addEventListener( "load", function(){
 
     // 1. a with rel=external opens in new window
     Array.from( document.querySelectorAll( 'a[rel*="external"]' ) ).forEach( function( $elt ) {
         $elt.setAttribute( "target", "_new" );
     } );
+
+    // 2. handle tabs
+    Array.from( document.querySelectorAll( "ul.nav.nav-tabs a" ) ).forEach( function( $elt ) {
+        $elt.addEventListener( "click", fHandleTab, "false" );
+    });
+
 } );
