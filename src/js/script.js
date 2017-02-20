a0dc12ab27fd543a26bd65f3d8d4d8eef32deb82
stephecloutier <stephecloutier@gmail.com>
2017-02-20 13:56:02 +0100

    Add trombinoscope

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,37 @@
 
 
-const fHandleTab = function ( oEvent ) {
-    let $this = $( this )
+const fHandleTab = function (oEvent) {
+    let $this = $(this)
 
     oEvent.preventDefault();
-    if ( $this.parent().hasClass( "active" ) ) {
+    if ($this.parent().hasClass("active")) {
         return;
     }
-    $( "ul.nav.nav-tabs .active" ).removeClass( "active" );
-    $this.parent().addClass( "active" );
-    $( ".tab-pane.active" ).removeClass( "active" );
-    $( `#${ $this.data( "tab-target" ) }` ).addClass( "active" );
+    $("ul.nav.nav-tabs .active").removeClass("active");
+    $this.parent().addClass("active");
+    $(".tab-pane.active").removeClass("active");
+    $(`#${$this.data("tab-target")}`).addClass("active");
 };
 
-$( function() {
+const fHandleTrombino = function() {
+    $trombinoFigures.filter(":visible").fadeOut(function(){
+        let $next = $(this).next();
+        if($next.length === 0){
+            $next = $trombinoFigures.first();
+        }
+        $next.fadeIn();
+    });
+}
+
+$(function() {
     // 1. a with rel-external
-    $( 'a[rel*="external"]' ).attr( "target", "_new" );
+    $('a[rel*="external"]').attr("target", "_new");
 
     // 2. tabs
-    $( "ul.nav.nav-tabs a" ).on( "click", fHandleTab );
-} );
+    $("ul.nav.nav-tabs a").on("click", fHandleTab);
+
+    // 3. trombinoscope
+    $trombinoFigures = $("#trombino figure");
+    $trombinoFigures.hide().first().show();
+    setInterval(fHandleTrombino, 1500);
+});
